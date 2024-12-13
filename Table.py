75131cca275b4cbfbbe1395ab27de06556e8da57
chaimamnsr <chaima.mansouri@edu.isetcom.tn>
2024-12-13 11:05:31 +0100

    gameisover

--- Table.py
+++ Table.py
@@ -14,9 +14,19 @@ class Table:
     def deal_to_players(self):
         for player in self.players:
             if player.is_active:
-                player.receive_card(self.deck.draw(1)[0])
-                player.receive_card(self.deck.draw(1)[0])
+                player.receive_card(self.deck.draw(2))
 
     def determine_winner(self):
-        # TODO
+        # TODO: Implémenter la logique de détermination du gagnant
         pass
+
+    """
+    Vérifie si la partie est terminée :
+    - Si un seul joueur a des jetons restants.
+    Retourne True si la partie est terminée, sinon False.
+    """
+    def GameIsOver(self):
+        remaining_players = [player for player in self.players if player.chips > 0]
+        if len(remaining_players) == 1:
+            return True
+        return False
