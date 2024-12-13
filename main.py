d544cd46b5712cf16976ed8acb0f1e6225938d87
teovlt <teo.villet2@gmail.com>
2024-12-13 10:41:30 +0100

    add comments on main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
 
     # Initialisation des joueurs
     players = [
-        Player(name=f"Joueur {i + 1}", chips=100) for i in range(3)  # Exemple avec 4 joueurs
+        Player(name=f"Joueur {i + 1}", chips=100) for i in range(3)
     ]
 
     # Demander nom du joueur et l'ajouter à l'array
@@ -20,18 +20,26 @@ def main():
     table.players = players
     table.deck.shuffle()
 
-    # TODO le jeu
-    # Afficher son jeu et la mise en cours (si pas premier tour pour la mise)
+    # Distribuer les cartes à chaque jouer
+
+    # Plus grosse boucle qui attend que tout le monde n'ai plus de jetons
+
+    # TODO boucle du jeu
     # Placer 3 cartes en milieu puis commencez les paris
+    # Sélectionner un premier joueur au hasard
     # Le premier joueur peut soit parier soit au dodo
     # Les joueurs suivant peuvent soient s'aligner soit au dodo
-    # Affichage de la première carte
+
+    # TOUR DU JOUEUR(si pas premier) : proposez 2 choix, s'aligner, se coucher et
+    # Afficher ses cartes en gros au dessus
+    # Si aligner, joueur suivant, sinon attendre la fin du tour
+
+    # A la fin du tour
+
+    # Affichage des cartes, méthode en affichant 3 puis 1 puis 1
     # Et de meme jusqua la fin
     # La boucle s'arrete lorsque il ne reste que un joueur en jeu ou si toutes les cartes sont révélés et que tt le monde a parié
-
-
-
-
+    # Dans ce cas la somme va a celui qui a la meilleure combinaison
 
     # Détermination du gagnant
     print("\n--- Résultats ---")
